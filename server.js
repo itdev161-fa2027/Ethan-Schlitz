8f82f56d019f444edec2d991dd693e7aa82fd131
Ethan Schlitz <schlitze@matc.edu>
2026-09-22 09:54:45 -0700

    Add user registration validation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator';
 import connectDatabase from './config/db.js';
 
 const app = express();
@@ -14,10 +15,23 @@ app.get('/', (req, res) => {
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post(
+  '/api/users',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+  }
+);
 const PORT = 3000;
 
 app.listen(PORT, () => {
