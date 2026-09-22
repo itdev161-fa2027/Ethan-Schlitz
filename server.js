83fa8db08a2f88bc448a0fef2626423673caebdc
Ethan Schlitz <schlitze@matc.edu>
2026-09-22 09:30:33 -0700

    Create user registration endpoint

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ import connectDatabase from './config/db.js';
 const app = express();
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('API Running');
 });
-
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
